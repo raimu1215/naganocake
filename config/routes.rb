a63135e70c2961c3d48b0d1d1975ca534308ef47
EC2 Default User <ec2-user@ip-172-31-1-172.ap-northeast-1.compute.internal>
2023-02-09 12:42:03 +0000

    [finish]データベース作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   
 # 顧客用
-root to: "homes#top"
-get 
 
 # URL /customers/sign_in ...
 devise_for :customers,skip: [:passwords], controllers: {
@@ -10,18 +8,18 @@ devise_for :customers,skip: [:passwords], controllers: {
   sessions: 'public/sessions'
 }
   scope module: :public do
+    root to: "homes#top"
     resources :items
   end
 
 # 管理者用
-root to: "homes#top"
-
 
 # URL /admin/sign_in ...
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
   namespace :admin do
+    root to: "homes#top"
     resources :items
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
