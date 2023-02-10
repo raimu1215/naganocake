fe589cc43854c2a99cf6f8ea9a36db40a6d73d8e
EC2 Default User <ec2-user@ip-172-31-1-172.ap-northeast-1.compute.internal>
2023-02-10 04:03:01 +0000

    [finish]about

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,14 @@ devise_for :customers,skip: [:passwords], controllers: {
 }
   scope module: :public do
     root to: "homes#top"
-    resources :items
+    get 'home/about' => 'homes#about', as: 'about'
+    resources :items, only: [:index, :show]
+    resources :registrations, only: [:new, :create]
+    resources :sessions, only: [:new, :create, :destroy]
+    resources :customers, only: [:show, :edit, :update, :withdrawal, :destroy]
+    resources :cart_items, only: [:index, :update, :destroy, :create]
+    resources :orders, only: [:new, :update, :index, :show]
+    resources :address, only: [:index, :edit, :create, :update, :destroy]
   end
 
 # 管理者用
@@ -20,7 +27,12 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
 }
   namespace :admin do
     root to: "homes#top"
-    resources :items
+    resources :sessions, only: [:new, :create, :destroy]
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :status, only: [:show, :update]
+    resources :making_status, only: [:update]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
