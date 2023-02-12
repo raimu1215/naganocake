77eb34438a2c9ecdd23f9f2402e8c1a7d72f92dc
EC2 Default User <ec2-user@ip-172-31-1-172.ap-northeast-1.compute.internal>
2023-02-12 01:03:56 +0000

    中断

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -2,4 +2,8 @@ class Admin::CustomersController < ApplicationController
     def index
         @customers = Customer.page(params[:page])
     end
+    
+    def show
+        @customer = Customer.find(params[:id])
+    end
 end
