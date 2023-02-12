3270a831c63d8558225d06a5d5aae6f076728855
EC2 Default User <ec2-user@ip-172-31-1-172.ap-northeast-1.compute.internal>
2023-02-12 13:42:58 +0000

    [finish]2/12

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -10,4 +10,10 @@ class Admin::CustomersController < ApplicationController
     def show
         @customer = Customer.find(params[:id])
     end
+    
+    def update
+      @customer = Customer.find(params[:id])
+      redirect_to admin_customer_path
+    end
+    
 end
