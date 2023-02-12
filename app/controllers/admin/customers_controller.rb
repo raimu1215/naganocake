30ea050a11e461ef731a25a5b98cf3d3f744ee02
EC2 Default User <ec2-user@ip-172-31-1-172.ap-northeast-1.compute.internal>
2023-02-12 00:19:27 +0000

    中断

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,2 +1,5 @@
 class Admin::CustomersController < ApplicationController
+    def index
+        @customers = Customer.page(params[:page])
+    end
 end
--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,6 +1,7 @@
 class Admin::ItemsController < ApplicationController
     def index
       @items = Item.all
+      @item = Item.page(params[:page])
     end
     
     def show
