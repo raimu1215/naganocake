6d121ee118a4740c2c72c8944d5bd8d2d25718c0
EC2 Default User <ec2-user@ip-172-31-1-172.ap-northeast-1.compute.internal>
2023-04-13 14:30:18 +0000

    finish

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,7 +1,6 @@
 class Admin::HomesController < ApplicationController
     def top
         @orders = Order.all
-        
         @order_history = Order.page(params[:page])
     end
 end
--- app/controllers/admin/status_controller.rb
+++ app/controllers/admin/status_controller.rb
@@ -1,6 +1,10 @@
 class Admin::StatusController < ApplicationController
     def show
-       @order = Order.all
+       @order = Order.find(params[:id])
+       @order_details = @order.order_details
+       
+       @total = @order_details.inject(0) { |sum, item| sum + item.sum_of_price }
+       
     end
     
 end
