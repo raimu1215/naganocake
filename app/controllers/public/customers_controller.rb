47364f3a73f7d08ea33fed74a45dd27b06d9f50a
EC2 Default User <ec2-user@ip-172-31-1-172.ap-northeast-1.compute.internal>
2023-04-19 14:11:43 +0000

    finish

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -11,7 +11,11 @@ class Public::CustomersController < ApplicationController
     
     def update
       @customer = Customer.find(params[:id])
-      redirect_to customer_path
+      if @customer.update(customer_params)
+         redirect_to customer_path(@customer.id)
+      else
+        render :edit
+      end
     end
     
     def withdrawal
@@ -20,5 +24,10 @@ class Public::CustomersController < ApplicationController
       reset_session
       flash[:notice] = "退会処理を実行いたしました"
       redirect_to root_path
+    end
+    
+    private
+    def customer_params
+      params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :telephone_number, :email)
     end
 end
